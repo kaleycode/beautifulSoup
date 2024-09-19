4d737f59ebb623e184992c33be6ffc46a2ccbe9e
kaleycode <readingkale@gmail.com>
2024-09-19 16:10:10 -0700

    Add files via upload

--- scraping_bookQuotes.py
+++ scraping_bookQuotes.py
@@ -1,11 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
-
-url = "https://bookroo.com/quotes/sherlock-holmes"
+url = "https://murder-mayhem.com/sherlock-holmes-quotes"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
-quotes = soup.find_all('div', class_ = 'text-lg md:text-xl leading-[1.8] whitespace-pre-wrap break-words mb-4')
+quotes = soup.find_all('p')
 for quote in quotes:
     print(quote.text)
-    print()
     print()
